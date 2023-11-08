8da83f3171315334a2a06864c0dbe14f17aa567a
natoles <mart.anatole@gmail.com>
2023-11-08 19:56:33 +0100

    Fixed compilation error + deleted temp files

--- Source/MegaloFuturae/Public/UI/MEGCardWidget.h
+++ Source/MegaloFuturae/Public/UI/MEGCardWidget.h
@@ -26,7 +26,7 @@ protected :
 
 	void FillCellWidgets();
 
-	TMap<enum EMEGCellPosition, UMEGCellWidget*> CellWidgets;
+	TMap<enum class EMEGCellPosition, UMEGCellWidget*> CellWidgets;
 	
 	UPROPERTY(meta = (BindWidget))
 	UMEGCellWidget* CellUL;
--- enc_temp_folder/7c6ded4275553da0e0d1e05065d4f51d/MEGCardWidget.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "UI/MEGCardWidget.h"
-
-void UMEGCardWidget::UpdateCard(int32 CardId)
-{
-}
